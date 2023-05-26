353f42fe46c21e9adf913433d9e577f3684353e3
asitpal211 <110338746+asitpal211@users.noreply.github.com>
2023-05-26 11:15:13 +0530

    Lets Scale the app to more users

--- localStorage.js
+++ localStorage.js
@@ -1,26 +1,34 @@
-function registerUser(){
-    // Retrieving data
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
 
-var name= document.getElementById("name").value;
-var email = document.getElementById("email").value; 
-var phone = document.getElementById("phone").value;
-var date = document.getElementById("date").value;
-var time = document.getElementById("time").value;
+    // Retrieve form input values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var date = document.getElementById("date").value;
+    var time = document.getElementById("time").value;
 
+    // Create an object to hold the user data
+    var userData = {
+      name: name,
+      email: email,
+      phone: phone,
+      date: date,
+      time: time
+    };
 
-// Storing data
-var userDetails = {
-    name: name,
-    email: email,
-    phone: phone,
-    date: date,
-    time: time
-    
-  };
-  
-  var userDetailsString = JSON.stringify(userDetails);
-  var uniqueKey = "userDetails" + Date.now(); // Appending a timestamp to create a unique key
-  localStorage.setItem(uniqueKey, userDetailsString);
+    // Retrieve existing user data from local storage or initialize an empty array
+    var existingData = JSON.parse(localStorage.getItem("userData")) || [];
 
+    // Add the new user data to the existing array
+    existingData.push(userData);
 
-}
+    // Store the updated user data in local storage
+    localStorage.setItem("userData", JSON.stringify(existingData));
+
+    // Clear the form inputs
+    document.getElementById("myForm").reset();
+
+    // Redirect to the home page
+    window.location.href = "home.html";
+  });
